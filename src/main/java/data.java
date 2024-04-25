6519712b3b727d015eb4f1deb10d24f7385784cf
บาบูนปู๊นๆ <130694465+hipposama@users.noreply.github.com>
2024-04-26 05:58:56 +0700

    v.1.0

---
+++ src/main/java/data.java
@@ -0,0 +1,17 @@
+import java.io.Serializable;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author DOM
+ */
+public class data implements Serializable{
+    int money ,headnum ,bodynum ,deck ,m_bank;
+    int buy1 ,buy2 ,buy3 ,buy4 ,buy5 ,buy6 ,buy7 ,buy8 ,buy9;
+    int code1 ,code2 ,code3 ,code4;
+    int sound ,effect;
+}
